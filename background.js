e8ff3fdf0f03981561387ec568e3624977d106bc
lanxisama <lanxisama@gmail.com>
2020-12-20 14:16:35 +0800

    remove audio record

--- background.js
+++ background.js
@@ -22,34 +22,36 @@ chrome.browserAction.onClicked.addListener(function(){
 
 
 async function init(){
-    // showVideo.style.display = "none";
-    // stop.removeAttribute("disabled");
-    let audioStream = await navigator.mediaDevices.getUserMedia({video: false,audio: true});     
+    //声音会undefined
+ 
+    // let audioStream = await navigator.mediaDevices.getUserMedia({video: false,audio: true});     
     let stream = await navigator.mediaDevices.getDisplayMedia({video: true,audio: true});
-    video.srcObject = stream;
-    stream.getVideoTracks().forEach(value => audioStream.addTrack(value));
+    if ("srcObject" in video) {
+        video.srcObject = stream
+    }else{
+        video.src = window.URL && window.URL.createObjectURL(stream) || stream
+    }
+ 
+    // stream.getVideoTracks().forEach(value => audioStream.addTrack(value));
 
-    // 此时的 audioStream 就有视频片段了
-    //var mediaRecorder = new MediaRecorder(stream[, options]);
-    //这个东西可能之后要改成全局
-    mediaRecorder = new MediaRecorder(audioStream,{type: "video/webm"}),
+    // mediaRecorder = new MediaRecorder(audioStream,{type: "video/webm"})
+    mediaRecorder = new MediaRecorder(stream,{type: "video/webm"}),
         chunks = [];
-    
-    // video.setAttribute("muted",true);
-    // video.setAttribute("autoplay",true);
     console.log(video,mediaRecorder)
-    debugger
+ 
     mediaRecorder.start();
     mediaRecorder.ondataavailable = function(e){ 
         chunks.push(e.data);
     };
     mediaRecorder.onstop = function(){ 
+        chrome.browserAction.setIcon({path:'./img/start.png'})
         handleStop(chunks);
     };
 
     // stop.onclick =  function(){ 
     //     mediaRecorder.stop();
     // };
+ 
 }
  
 function removeStop(){
