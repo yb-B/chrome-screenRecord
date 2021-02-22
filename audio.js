67a1c620848f5583bde1ee88c0442218f9386b0f
lanxisama <lanxisama@gmail.com>
2021-02-22 17:59:34 +0800

    micophone fixed

--- audio.js
+++ audio.js
@@ -1,32 +1,20 @@
 
-let option = {
+const options = {
     audio:true
+} 
+
+async function getSources(){
+    // 这个是可以正常获取到的 在当前iframe页面中
+    //接下来需要把这个传递给background.js
+    const micsource = await navigator.mediaDevices.getUserMedia(options);
+    console.log(micsource) 
+    chrome.runtime.sendMessage('micsource',{
+        type:'MicSource',
+        msg:micsource
+    },function(response){
+        console.log(response);
+    })
 }
 
-// function getSource(){
 
-// }
-
-navigator.permissions.query({ name: 'microphone'/*and 'camera'*/ }).then(data=>{
-    console.log(data.state)
-})
-
-navigator.mediaDevices.getUserMedia(option).then(audiostream=>{
-    console.log(audiostream)
-    // chrome.runtime.sendMessage({
-    //     type:"audio-stream",
-    //     stream:audiostream
-    // })
-    audiostream = JSON.parse(JSON.stringify(audiostream))
-    window.postMessage(audiostream,chrome.extension.getURL('background.html'));
-    chrome.runtime.send({
-        type:'audio-load',
-        stream:audiostream
-    })
-}).catch(err=>{
-    console.log(err,err.name)
-})
-
-// content scirpt再页面开始时候向页面注入一个iframe,同时这个iframe中执行一个脚本用来获取mediaStream,这个流怎么样才可以让background.js获取到
-// 利用window.postMessage和chrome.runtime.sendMessag都无法获取，不知道是否需要额外操作
- 
+getSources();
--- background.js
+++ background.js
@@ -1,103 +1,107 @@
-
+const audioCtx = new AudioContext();
+const destination = audioCtx.createMediaStreamDestination();
 
 var video = document.createElement("video");
 var mediaRecorder;
 var isRecord = false;
-
-const changeStop = ()=>{
-    chrome.browserAction.setIcon({path:'./img/stop.png'})
+var micstream;
+var micsource;
+const output = new MediaStream();
+const changeStop = () => {
+    chrome.browserAction.setIcon({ path: './img/stop.png' })
 }
 
-const changeStart = ()=>[
-    chrome.browserAction.setIcon({path:'./img/start.png'})
+const changeStart = () => [
+    chrome.browserAction.setIcon({ path: './img/start.png' })
 ]
-chrome.browserAction.onClicked.addListener(function(){
-    if(!isRecord){
+chrome.browserAction.onClicked.addListener(function () {
+    if (!isRecord) {
         isRecord = !isRecord;
         //这地方应该是异步修改图标
         changeStop();
-        chrome.runtime.sendMessage({type:'request-audio'})
+        chrome.runtime.sendMessage({ type: 'request-audio' })
         return init()
-    }else{
+    } else {
         isRecord = !isRecord;
         changeStart();
         return mediaRecorder.stop();
     }
 });
- 
+
 
 //https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API
 
 
-async function init(){
+async function init() {
     //获取可用设备列表
     let devices = await navigator.mediaDevices.enumerateDevices();
-    let audioStream;
+    
+    micdevices = devices.filter(m=>m.kind=='audioinput')
+    // console.log(micdevices)
+    // let audioStream;
+    let constraints = {
+        audio:{
+            deviceId:micdevices[1].deviceId
+        }
+    }
     let stream;
     //提示用户选择显示器或显示器的一部分（例如窗口）以捕获为MediaStream 以便共享或记录。返回解析为MediaStream的Promise。
-    //这个想当于只打开了录屏 没有打开录音功能
-    // audioStream = await navigator.mediaDevices.getUserMedia({audio:true})
-    try{
-        stream = await navigator.mediaDevices.getDisplayMedia({video: true,audio: true});
-    }catch{
+    try {
+        const mic = await navigator.mediaDevices.getUserMedia(constraints);
+        micstream = mic;
+        micsource = audioCtx.createMediaStreamSource(mic); //创建音频流
+ 
+        micsource.connect(destination);
+        stream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
+        // output.addTrack(destination.stream.getAudioTracks()[0]) //把麦克风声轨传入
+        output.addTrack(micsource.mediaStream.getAudioTracks()[0]) //把麦克风声轨传入
+        output.addTrack(stream.getVideoTracks()[0]) //把录制视频传入
+
+
+    } catch (e) {
+        console.log(e)
         isRecord = false;
         changeStart();
     }
-    console.log(stream)
-    if(stream){
-        if ("srcObject" in video) {
-            video.srcObject = stream
-        }else{
-            video.src = window.URL && window.URL.createObjectURL(stream) || stream
-        }
-
-        
-         stream.getVideoTracks().forEach(value => audioStream.addTrack(value));
-        
-        // mediaRecorder = new MediaRecorder(audioStream,{type: "video/webm"});
-        let  chunks = []
-        let mediaConstraints = {
-            mimeType: 'video/webm;codecs=vp8,opus'
-            // ,bitsPerSecond:1000
-        }
-        mediaRecorder = new MediaRecorder(stream,mediaConstraints),
-            // chunks = [];
-    
-        mediaRecorder.start();
-        mediaRecorder.ondataavailable = function(e){ 
-            chunks.push(e.data);
-        };
-        mediaRecorder.onstop = function(){ 
-            chrome.browserAction.setIcon({path:'./img/start.png'})
-            handleStop(chunks);
-        };
-
-        // stop.onclick =  function(){ 
-        //     mediaRecorder.stop();
-        // };
+            
+    if ("srcObject" in video) {
+        video.srcObject = output
+    } else {
+        video.src = window.URL && window.URL.createObjectURL(output) || output
     }
+    let chunks = []
+    let mediaConstraints = {
+        mimeType: 'video/webm;codecs=vp8,opus'
+        // ,bitsPerSecond:1000
+    }
+    mediaRecorder = new MediaRecorder(output, mediaConstraints),
 
- 
+    mediaRecorder.start();
+    
+    mediaRecorder.ondataavailable = function (e) {
+        if(e.data && e.data.size >0){
+            chunks.push(e.data);
+        }
+    };
+    mediaRecorder.onstop = function () {
+        chrome.browserAction.setIcon({ path: './img/start.png' })
+        handleStop(chunks);
+    };
 }
- 
-function removeStop(){
-    stop.setAttribute("disabled",true);
+
+function removeStop() {
+    stop.setAttribute("disabled", true);
 }
-function handleStop (chunks){
+function handleStop(chunks) {
 
     var tracks = video.srcObject.getVideoTracks();
     tracks.forEach(track => track.stop());
     video.srcObject = null;
-
-    // showVideo.style.display = "inline-block";
-    // stop.setAttribute("disabled",true);
-    // removeStop()
-
-    var blob = new Blob(chunks,{'type': 'video/mp4'});
+    var blob = new Blob(chunks, { 'type': 'video/mp4' });
     let myUrl = URL.createObjectURL(blob);
     //下载blob只能通过 a标签点击实现
     var link = document.createElement('a');
-                
+
     link.style.display = 'none';
     link.href = myUrl;
     link.setAttribute('download', 'record.mp4');
